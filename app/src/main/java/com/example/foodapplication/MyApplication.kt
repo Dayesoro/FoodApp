6a7320130dbf661d35a578480b902b95ef19887e
Daniel Ayesoro <dayesoro@gmail.com>
2022-12-27 15:25:52 +0000

    Created MyApplication class where hilt-dagger components should be generated

---
+++ app/src/main/java/com/example/foodapplication/MyApplication.kt
@@ -0,0 +1,8 @@
+package com.example.foodapplication
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication: Application() {
+}
---
+++ app/src/main/java/com/example/foodapplication/RemoteDataSource.kt
@@ -0,0 +1,4 @@
+package com.example.foodapplication
+
+class RemoteDataSource() {
+}

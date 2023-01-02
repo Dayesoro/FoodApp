3b6a337fbdcf90d7e2c930f6b2f0ec8587c471dd
Daniel Ayesoro <dayesoro@gmail.com>
2023-01-02 18:55:44 +0000

    Injected RemoteDataSource with FoodRecipes api

--- app/src/main/java/com/example/foodapplication/RemoteDataSource.kt
+++ app/src/main/java/com/example/foodapplication/RemoteDataSource.kt
@@ -1,4 +1,14 @@
 package com.example.foodapplication
 
-class RemoteDataSource() {
+import com.example.foodapplication.models.FoodRecipe
+import retrofit2.Response
+import javax.inject.Inject
+
+class RemoteDataSource @Inject constructor(
+    private val foodRecipesApi: FoodRecipesApi
+) {
+
+    suspend fun getRecipes(queries: Map<String, String>): Response<FoodRecipe>{
+        return foodRecipesApi.getRecipes(queries)
+    }
 }

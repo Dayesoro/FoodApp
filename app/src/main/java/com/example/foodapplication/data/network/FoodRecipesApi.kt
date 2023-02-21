28d0d1c7341b202646e8973104c27a3948d8d9ff
Daniel Ayesoro <dayesoro@gmail.com>
2023-02-21 13:09:49 +0000

    added network class to handle response from the API and added viewmodel class, also implemented functions in the viewmodel to make the network calls

--- app/src/main/java/com/example/foodapplication/data/network/FoodRecipesApi.kt
+++ app/src/main/java/com/example/foodapplication/data/network/FoodRecipesApi.kt
@@ -4,7 +4,9 @@ import com.example.foodapplication.models.FoodRecipe
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.QueryMap
-
+/**
+ * This interface defines how Retrofit talks to the web server using HTTP requests
+ */
 interface FoodRecipesApi {
     @GET("/recipes/complexSearch")
     suspend fun getRecipes(
